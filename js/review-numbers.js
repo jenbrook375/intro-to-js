d78f54c2cb52c272a1930a7f274e6eb6e0bf9f01
JenBrook <jenbrook375@hotmail.com>
2019-04-25 13:27:41 -0700

    added review-numbers.js

---
+++ js/review-numbers.js
@@ -0,0 +1,23 @@
+// creates number variables
+var a = 8;
+var b = 26;
+
+// creates variable 'sum'
+var sum = a + b;
+console.log(sum);
+
+// creates variable 'diff'
+var diff = a - b;
+console.log(diff);
+
+// creates variable 'mult'
+var mult = a * b;
+console.log(mult);
+
+// creates variable 'divide'
+var divide = b / a;
+console.log(divide);
+
+// creates variable 'mod'
+var mod = b % a;
+console.log(mod);
