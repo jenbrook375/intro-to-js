ffca8c3e17fc7a7f1f378de4e0014002d118df08
JenBrook <jenbrook375@hotmail.com>
2019-04-25 14:00:15 -0700

    added string concatenation

--- js/review-strings.js
+++ js/review-strings.js
@@ -1,5 +1,12 @@
-// creates string variable
+// creates string variable hello
 var hello = 'Hello World!';
 console.log(hello);
-console.log(hello.split(" ")[0]);
-console.log(hello.split(" ")[1]);
+console.log(hello.split(" ")[0]); // displays Hello (string in index 0)
+console.log(hello.split(" ")[1]); // displays World! (string in index 1)
+
+
+var make = 'Dodge';
+var model = 'Grand Caravan';
+var makeModel = make +  ' ' + model;
+console.log(make + ' ' + model);
+console.log(makeModel);
