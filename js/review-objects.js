2c18b5fea20cfda685b9e2f865f859da181cbe49
JenBrook <jenbrook375@hotmail.com>
2019-04-26 16:57:22 -0700

    added review of objects, practiced concatenatinf kvp

---
+++ js/review-objects.js
@@ -0,0 +1,8 @@
+// create a name object  with first/last kvp
+var fullName = {
+    firstName: 'Jennifer ',
+    lastName: 'Brookshire',
+};
+
+console.log(fullName); // displays object
+console.log(fullName.firstName + fullName.lastName); // displays concatenated kvp's
