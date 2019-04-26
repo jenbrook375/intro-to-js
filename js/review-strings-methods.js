df1706ae4296c022aba4c84f975598af561fb6e1
JenBrook <jenbrook375@hotmail.com>
2019-04-26 16:27:24 -0700

    learning concatenation

--- js/review-strings-methods.js
+++ js/review-strings-methods.js
@@ -7,3 +7,10 @@ console.log(hello.split(' ')[0]); // display Hello only
 console.log(hello.slice(6, 11)); // display World only
 console.log(hello.substring(6, 11)); // another way to display only world
 console.log(hello.substring(6)); // display all indices after specified index
+
+var make = 'Dodge';
+var model = 'Grand Caravan';
+console.log(make + ' ' + model); //concatenate variables with a space in between
+
+var makeModel = make + ' ' + model;
+console.log(makeModel);
